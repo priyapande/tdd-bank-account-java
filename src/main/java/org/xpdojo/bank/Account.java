c7cd23fbc7a1707100a057d14cf991169e239e9b
priyapande <priyapande07@gmail.com>
2020-08-06 15:50:08 +0530

    added behaviour for deposit and balance in account

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
+
+    public int balance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance+=amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void newAccountShouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(20);
     }
 }
